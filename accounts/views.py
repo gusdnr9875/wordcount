9fe93bc91f77f648073898ed5fa9d0e432911874
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2019-05-24 16:04:35 +0900

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        # User has info and wants an account now!
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'signup.html', {'error': 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('blog')
+        else:
+            return render(request, 'signup.html', {'error': 'Passwords must match'})
+    else:
+        # User wants to enter info
+        return render(request, 'signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
